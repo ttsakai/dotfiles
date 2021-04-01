f3dd67d9b0ec82849cabbab647886473ba0895d9
masterkuzutt <masterkuzutt@gmail.com>
2021-04-01 20:40:52 +0900

    Add fish config

---
+++ config.fish
@@ -0,0 +1,17 @@
+set -g theme_newline_cursor yes
+set -g theme_display_git_master_branch yes
+set -g theme_color_scheme dracula
+set -g theme_powerline_fonts no
+set -g theme_display_date no
+set -g theme_display_cmd_duration no
+set PATH /usr/local/bin /usr/sbin $HOME/.cargo/bin $PATH
+set EDITOR nvim
+set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+powerline-setup
+
+
+
+export VISUAL=nvim
+
+alias vim=nvim
+alias vi=nvim
