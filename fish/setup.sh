6efa8f813b7cac30625290aa8db3181b6385a75a
masterkuzutt <masterkuzutt@gmail.com>
2021-09-05 10:51:46 +0900

    Modify theme setting, update setup

--- fish/setup.sh
+++ fish/setup.sh
@@ -1,4 +1,8 @@
 
 
+
 # install fisherman
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+rm  ~/.config/fish/config.fish
+ln -s  $ROOT_DIR/fish/config.fish ~/.config/fish/config.fish
