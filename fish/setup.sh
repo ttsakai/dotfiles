b5a8283f9a092bed61b1592984d60e464ff9f458
masterkuzutt <masterkuzutt@gmail.com>
2021-09-12 08:13:39 +0900

    add to-fish

--- fish/setup.sh
+++ fish/setup.sh
@@ -1,8 +1,13 @@
-
-
-
 # install fisherman
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
 rm  ~/.config/fish/config.fish
 ln -s  $ROOT_DIR/fish/config.fish ~/.config/fish/config.fish
+
+# install fisherman plugin
+fisher install oh-my-fish/oh-my-fish
+# fisher install oh-my-fish/theme-batman
+fisher install oh-my-fish/theme-agnoster
+fisher install jorgebucaran/fisher
+fisher install joehillen/to-fish
+
