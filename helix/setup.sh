28ae470492129ee7463e4c3d19e97b3db7318d93
ttsakai <masterkuzutt@gmail.com>
2022-03-29 09:28:36 +0900

    Comment out lsp, mod setup for building from src

--- helix/setup.sh
+++ helix/setup.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
 FILENAME=languages.toml
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+timestamp=$(date +%Y%m%d%H%M%S)
+tmpd=$(mktemp -dt "$basepath.$timestamp.$$")/
+cd $tmpd
 
 wget https://raw.githubusercontent.com/helix-editor/helix/master/${FILENAME}
-mv ${FILENAME} ~/.config/helix/${FILENAME}
+cp ${FILENAME} ~/.config/helix/${FILENAME}
+cp $SCRIPT_DIR/config.toml ~/.config/helix/config.toml
+
+git clone git@github.com:helix-editor/helix.git
+cp -r runtime/ ~/.config/helix/runtime/
+hx --grammar fetch
+hx --grammar build
