d94a327fac05452d896c6660d1ef5f8b302564bf
ttsakai <masterkuzutt@gmail.com>
2024-07-10 14:24:06 +0900

    mod misc

--- fish/setup.sh
+++ fish/setup.sh
@@ -1,13 +1,16 @@
 # install fisherman
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+# 動かなかったのでコメントアウトしてる
+# curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+# 参考資料 https://zenn.dev/toipptakosan11/articles/aa42053d836bdf
 
 rm  ~/.config/fish/config.fish
 ln -s  $ROOT_DIR/fish/config.fish ~/.config/fish/config.fish
 
+# theme系のpluginもういらないのでコメントアウト。
 # install fisherman plugin
-fisher install oh-my-fish/oh-my-fish
+# fisher install oh-my-fish/oh-my-fish
 # fisher install oh-my-fish/theme-batman
-fisher install oh-my-fish/theme-agnoster
-fisher install jorgebucaran/fisher
+# fisher install oh-my-fish/theme-agnoster
+# fisher install jorgebucaran/fisher
 fisher install joehillen/to-fish
 
